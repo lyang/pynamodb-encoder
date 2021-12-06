908234a08f4ef68b0a85a359f79c521b2119f484
Lin Yang <github@linyang.me>
2021-12-06 11:40:15 -0800

    Add tests for decoding list attribute

--- tests/decoder_test.py
+++ tests/decoder_test.py
@@ -1,5 +1,10 @@
 import pytest
-from pynamodb.attributes import BinaryAttribute, NumberAttribute, UnicodeAttribute
+from pynamodb.attributes import (
+    BinaryAttribute,
+    ListAttribute,
+    NumberAttribute,
+    UnicodeAttribute,
+)
 from pynamodb.models import Model
 
 from pynamodb_encoder.decoder import Decoder
@@ -41,3 +46,16 @@ def test_decode_binary_attribute(decoder):
 
     assert pet.name == "Garfield"
     assert pet.weight == bytes([40])
+
+
+def test_decode_list_attribute(decoder):
+    class Pet(Model):
+        name = UnicodeAttribute()
+        age = NumberAttribute()
+        likes = ListAttribute()
+
+    pet = decoder.decode(Pet, {"name": "Garfield", "age": 43, "likes": ["lasagna"]})
+
+    assert pet.name == "Garfield"
+    assert pet.age == 43
+    assert pet.likes == ["lasagna"]
